f245bbe6d4c067b350388a418515650215946489
Julio <batistatorresjulio@gmail.com>
2023-10-10 05:01:03 +0200

    order_processor

--- ProcessOrder/views.py
+++ ProcessOrder/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 import json
+
+
+
 class order_processor(APIView):
         
         
@@ -13,14 +15,28 @@ class order_processor(APIView):
                         criterion=json.loads(request.body).get('criterion')
                   
                 except:
-                        return  Response('the Json content is wrowng.......')
+                        return  Response('the Json content is wrong.......')
              
-                
-                if criterion not in  ['completed','canceled','pending']:
-                        return Response('The criterion dont exisit......')
+                  
                 for order in orders:
                         if order['price']<=0:
                                 return Response(' the price of the object '+order['item']+" is incorrect....." )
+                        
+                        
+                if criterion =='all':
+                        for order in orders:
+                                object_price=0
+                
+                                object_price=order['quantity']*order["price"]                
+
+                                mount+=object_price
+
+                        return Response(mount)
+                
+                
+                if criterion not in  ['completed','canceled','pending']:
+                        return Response('The criterion dont exisit......')
+              
                  
                 
                 for order in orders:
